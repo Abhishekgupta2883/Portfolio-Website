7b62a3f47b044874f05a44d5d2560015750d9dd7
Abhishek Gupta <ag8882138469@gmail.com>
2025-04-14 14:37:40 +0530

    connecting

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,10 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: process.env.PORT || 3000,
-    host: '0.0.0.0'
+    host: '0.0.0.0',
+    allowedHosts: [
+      'portfolio-website-70dn.onrender.com',
+      'localhost'
+    ]
   }
 })
